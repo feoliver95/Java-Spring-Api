712b37fbef62f49b3abbd3026ad3ba8f04785563
feoliver95 <feoli.jobs@gmail.com>
2020-06-29 11:31:08 -0300

    atualizacao

--- src/main/java/com/meudominio/api/JavaRestApiApplication.java
+++ src/main/java/com/meudominio/api/JavaRestApiApplication.java
@@ -1,5 +1,6 @@
 package com.meudominio.api;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,5 +12,7 @@ public class JavaRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRestApiApplication.class, args);
 	}
+	
+
 
 }
